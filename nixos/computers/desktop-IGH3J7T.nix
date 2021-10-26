810082b3e2a397a79e1fc254bf134150d287a38a
gyroskan <gyroskan@gmail.com>
2021-10-26 22:41:36 +0200

    desktop config
    Signed-off-by: gyroskan <gyroskan@gmail.com>

--- nixos/computers/desktop-IGH3J7T.nix
+++ nixos/computers/desktop-IGH3J7T.nix
@@ -1,3 +1,22 @@
 # Config file for desktop computer.
 
-:
+{ config, pkgs, lib, ... }:
+
+{
+  fileSystems = 
+  {
+    "/mnt/windows/ssd" = 
+    {
+      device = "/dev/disk/by-uuid/42145AD1145AC815";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000"];
+    };
+
+    "/mnt/windows/hdd" = 
+    {
+      device = "/dev/disk/by-uuid/8A2813A828139275";
+      fsType = "ntfs";
+      options = [ "rw" "uid=1000" ];
+    };
+  };
+}
