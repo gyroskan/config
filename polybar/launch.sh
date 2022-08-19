d3811f5c848e5e271fe9ebec208059973e9bb3a8
baptiste.nehme <gyroskan@gmail.com>
2022-08-19 23:54:06 +0200

    feat(polybar): added bar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,7 @@ DIR="$HOME/.config/polybar/forest"
 
 # Terminate already running bar instances
 killall -q polybar
+killall -q .polybar-wrapper
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
